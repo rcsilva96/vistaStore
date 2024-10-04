931dd26c1b28de4bf6be181937b659483626bf4f
Renato Carvalho Silva <rsilv1822@gmail.com>
2024-10-04 00:06:25 -0300

    finished base project

--- vistaStore/src/application/Program.java
+++ vistaStore/src/application/Program.java
@@ -24,8 +24,22 @@ public class Program {
         System.out.println("Por favor, insira a quantidade do produto em estoque: ");
             product.quantity = scan.nextInt();
 
-        System.out.println(product);
+        System.out.println("Dados do produto: " + product);
 
+        System.out.println();
+        System.out.println("Entre com o número de produtos a serem adicionados no estoque: ");
+        int quantity = scan.nextInt();
+        product.addProduto(quantity);
+        System.out.println("Dados atualizados do produto: " + product);
+
+        System.out.println("Entre com o número de produtos a serem removidos do estoque: ");
+        quantity = scan.nextInt();
+        if (quantity <= product.quantity) {
+            product.removeProduto(quantity);
+            System.out.println("Dados atualizados do produto: " + product);
+        } else {
+            System.out.println("ERRO FATAL: A quantidade a ser removida não pode ser maior que o valor em estoque. Encerrando o sistema...");
+        }
         scan.close();
     }
 }
